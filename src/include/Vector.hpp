b69378f87fe0223d62bd2716885339c292b13699
PolyMj <ballarmj@sunypoly.edu>
2024-01-30 17:07:39 -0500

    Vector.hhp finished?

---
+++ src/include/Vector.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+namespace potato {
+	template<typename T>
+	struct VecLength {using type = float};
+
+	template<>
+	struct VecLength<double> {
+		using type = double;
+	};
+
+
+	template<typename T>
+	struct Vec3 {
+		union { T x{}, r, s; };
+		union { T y{}, g, t; };
+		union { T z{}, b, p; }
+
+		Vecr() = default;
+		Vec3(T x, T y, T z) : x(x), y(y), z(z) {};
+
+		auto length() -> typename VecLength<T>::type {
+			return sqrt(static_cast<typename VecLength<T>::type>(
+				x*x + y*y + z*z
+			))
+		};
+
+		template<typename U> 
+		auto operator+(Vec3<U> v) const -> Vec3<decltype(T{} + U{})> {
+			return {
+				this.x + v.x,
+				this.y + v.y,
+				this.z + v.z
+			};
+		};
+
+		friend ostream& operator<<(ostream& os, Vec3<T v) {
+			os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+			return os;
+		};
+
+		T& operator[](int i) {
+			if(i < 0 || i >= 3) 
+				throw std::out_of_range("ERROR: " + to_string(i) + " out of range!");
+			switch(i) {
+				case 0:
+					return x;
+				case 1:
+					return y;
+				case 2:
+					return z;
+			}
+
+			return -1;
+		};
+
+
+	};
+
+	using Vec3f = Vec3<float>;
+	using Vec3i = Vec3<int>;
+	using Vec3d = Vec3<double>;
+
+
+
+
+
+};
