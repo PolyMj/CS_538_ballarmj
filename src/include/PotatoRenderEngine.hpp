8ea92358931f828d73c2f660920aef49f7e36602
PolyMj <ballarmj@sunypoly.edu>
2024-02-10 22:55:39 -0500

    Creation & basic reconfig of files

---
+++ src/include/PotatoRenderEngine.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <cmath>
+#include <GL/glew.h>					
+#include <GLFW/glfw3.h>
+#include "Vector.hpp"
+#include "Buffer.hpp"
+#include "Settings.hpp"
+#include "Timing.hpp"
+using namespace std;
+using namespace potato;
+
+class PotatoRenderEngine {
+    private:
+        // Data for display buffer
+        unsigned int windowTextureID = 0;        
+        Image<Vec3u> *frontBuffer = 0;
+        Image<Vec3u> *screenBuffer = 0;
+
+        int windowWidth = 0;
+        int windowHeight = 0;
+        
+        // BONUS    
+        int currentCol = 0;
+
+        // Timing stuff        
+        Timekeeper timekeeper;
+        
+        // Internal drawing functions        
+        void drawOneFrame();    
+        void drawAABox( Image<Vec3u> *buffer,
+                        int sx, int sy, 
+                        int ex, int ey,
+                        Vec3u color);
+        
+    public:
+        PotatoRenderEngine(int windowWidth, int windowHeight);
+        ~PotatoRenderEngine();  
+        // Copies display buffer to window texture  
+        void renderToWindowTexture();
+};
